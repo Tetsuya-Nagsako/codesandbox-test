2ca98fa5d1d4aa67ae7e3c766bd98b443b79f320
Tetsuya-Nagsako <76085885+Tetsuya-Nagsako@users.noreply.github.com>
2021-09-16 20:54:32 +0900

    lesson1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
-import "./styles.css";
+// const func = (str) => {
+//   return str;
+// };
+// console.log(func('koko'));
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const sum = (num1, num2) => num1 + num2 ;
+
+// console.log(sum(1,2));
+
+// const myProfile = {
+//   name: 'jake',
+//   age: 28,
+// };
+
+// const mesa = `name is ${myProfile.name}. Iam ${myProfile.age}`;
+//   console.log(mesa);
+
+//   const { name, age } = myProfile;
+// const mesa2 = `name is ${name}. Iam ${age}`;
+// console.log(mesa2);
+
+// const myProfile = ['jake', 20];
+
+// const mesa3 = `name is ${myProfile[0]}. age is ${myProfile[1]}`;
+// // console.log(mesa3);
+
+// const [name, age] = myProfile;
+
+// const mesa4 = `name is ${name}. age is ${age}`;
+// console.log(mesa4);
+
+// const sayHello = (name ) => console.log(`hello ${name} !`);
+// sayHello(123);
+
+// const arr1 = [1,2,3];
+// // console.log(arr1);
+// console.log(...arr1);
+
+// const sum = (num1, num2, num3) => console.log(num1 + num2 + num3);
+
+// sum(...arr1);
+
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(arr3);
+
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// console.log(arr8);
+
+// arr8[0] = 100;
+// console.log(arr4);
